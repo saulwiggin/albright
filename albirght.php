3f0993af2df23e10eeec6cdec5e34176cc8129c9
Saul Wiggin <saulwiggin@googlemail.com>
2014-12-09 20:46:40 +0000

    front end design

---
+++ albirght.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+
+<table>
+<tr> <th> filing date </th><th> priority date </th><th> foreign filings deadline </th> <th> first renewal deadline </th> </tr>
+<tr> </tr>
+</table>
+
+</body>
